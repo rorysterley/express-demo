7276d4214b0d0660455539a2a3d8f0ab05b02a2b
Rory Sterley <rorysterley@gmail.com>
2014-12-08 11:06:11 -0800

    Fix authentication

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ app.use(express.static('public'));
 var redis = require('redis');
 
 if (process.env.REDISTOGO_URL) {
-  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var rtg = require("url").parse(process.env.REDISTOGO_URL);
   var client = redis.createClient(rtg.port, rtg.hostname);
-
-  client.redis.auth(rtg.auth.split(":")[1]);
+  
+  client.auth(rtg.auth.split(":")[1]);
 } else {
   var client = redis.createClient();
   // Status: production, development, test
